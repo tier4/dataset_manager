612526d0b53731a96697cac063e53be2c6353e71
Yuma <yuma.nihei@tier4.jp>
2019-12-17 23:45:11 +0900

    first commit
    Signed-off-by: Yuma <yuma.nihei@tier4.jp>

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# please set environmental variable "AW_DATASET_PATH" in ~/.bashrc
+source ~/.bashrc
+
+if [ -z $AW_DATASET_PATH ];then
+  echo "please set environment variable AW_DATASET_PATH"
+elif [ ! -d $AW_DATASET_PATH ];then
+  echo "cannot find $AW_DATASET_PATH"
+else
+  roscd dataset_manager || {
+    echo "failed to change directory"
+    return
+  }
+  if [ -e link ];then
+    rm -rf link
+  fi
+  ln -s $AW_DATASET_PATH link
+  echo "$AW_DATASET_PATH"
+  echo "success to install dataset!!!"
+fi
